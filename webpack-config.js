57cf955a7a9302fb74cb71a2439ae6fb611ac31a
Yazid Ismail <flight846@gmail.com>
2018-01-09 01:04:26 +0800

    Add webpack config

---
+++ webpack-config.js
@@ -0,0 +1,15 @@
+modeule.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
